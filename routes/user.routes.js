0697b778a6b7298b2fd355570d1286d92d2c006e
Jessy BREYNE <jessybreyne@protonmail.com>
2021-05-20 20:09:54 +0200

    Crud of user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,5 +7,9 @@ router.post("/register", authController.signUp);
 // user DB
 router.get("/", userController.getAllUsers);
 router.get('/:id', userController.userInfo);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+router.patch('/follow/:id', userController.follow);
+router.patch('/unfollow/:id', userController.unfollow);
 
 module.exports = router;
