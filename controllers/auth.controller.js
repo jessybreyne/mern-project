e6af6b41bc8e3b654e8cc7bdc1c42838f8508c58
Jessy BREYNE <jessybreyne@protonmail.com>
2021-05-21 11:37:11 +0200

    Authentification

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,16 @@
 const UserModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const { signUpErrors, signInErrors } = require('../utils/errors.utils');
+
+const maxAge = 3 * 21 * 60 * 60 * 1000;
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.TOKEN_SECRET, {
+        expiresIn: maxAge
+    })
+}
 
 module.exports.signUp = async (req, res) => {
-    console.log(res)
     const { pseudo, email, password } = req.body;
 
     try {
@@ -9,6 +18,26 @@ module.exports.signUp = async (req, res) => {
         res.status(201).json({ user: user._id });
     }
     catch (err) {
-        res.status(200).send({ err });
+        const errors = signUpErrors(err);
+        res.status(200).send({ errors });
     }
+}
+
+module.exports.signIn = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await UserModel.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge })
+        res.status(200).json({user: user._id});
+    } catch (err) {
+        const errors = signInErrors(err);
+        res.status(200).send({ errors });
+    }
+}
+
+module.exports.logout = (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
 }
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
-require('dotenv').config({path: './config/.env'});
+require('dotenv').config({ path: './config/.env' });
 require('./config/db');
+const { checkUser, requireAuth } = require('./middleware/auth.middleware');
 const app = express();
 
 app.use(
-  express.urlencoded({
-    extended: true
-  })
+    express.urlencoded({
+        extended: true
+    })
 )
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.json())
+// jwt
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id);
+});
 
 // routes
 app.use('/api/user', userRoutes);
