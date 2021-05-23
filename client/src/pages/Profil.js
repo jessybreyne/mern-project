1e10f9ea6d1289f169019efa76c3d5ef2e87cad0
Jessy BREYNE <jessybreyne@protonmail.com>
2021-05-23 13:08:29 +0200

    Formulaire inscription et connexion

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Log from '../components/Log';
+import { UidContext } from "../components/AppContext";
 
 function Profil() {
+    const uid = useContext(UidContext);
+
     return (
-        <div>
-            <div className="profil-page">
+        <div className="profil-page">
+            {uid ? (
+                <h1>UPDATE PAGE</h1>
+            ) : (
                 <div className="log-container">
                     <Log signin={false} signup={true} />
                     <div className="img-container">
                         <img src="./img/log.svg" alt="img-log"/>
                     </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
