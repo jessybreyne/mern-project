8a36c5a9a0f7e2a55dfc8c5459ea8d9cf414a2ab
Jessy BREYNE <jessybreyne@protonmail.com>
2021-05-23 17:24:49 +0200

    Possibilité de changer sa photo de profil

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -6,15 +6,15 @@ module.exports.checkUser = (req, res, next) => {
     if (token) {
         jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
             if (err) {
-                res.locals.user = null;
-                res.cookie('jwt', '', { maxAge: 1 });
-                next();
+              res.locals.user = null;
+              // res.cookie('jwt', '', { maxAge: 1 });
+              next();
             } else {
-                console.log('decoded token ', decodedToken);
-                let user = await UserModel.findById(decodedToken.id);
-                res.locals.user = user;
-                console.log(res.locals.user);
-                next();
+              console.log("decoded token ", decodedToken);
+              let user = await UserModel.findById(decodedToken.id);
+              res.locals.user = user;
+              console.log(res.locals.user);
+              next();
             }
         })
     } else {
